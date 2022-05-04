b26d663f17e8617a716ab0bc690a17b37362cc76
Alew <xalejandrow@gmail.com>
2022-05-04 17:38:42 +0000

    condicionales

---
+++ python introduction/condicionales.py
@@ -0,0 +1,14 @@
+estado_de_relación = "casada"
+nombre = "Taylor"
+apellido = "Swift"
+
+if estado_de_relación == "soltera":
+    print("Miss "+nombre + " " + apellido) # imprime Miss Taylor Swift
+else:
+    print("Mrs "+nombre + " " + apellido) # imprime Mrs Taylor Swift
+
+numero = 20
+if numero < 18:
+    print("Esto se mostrará si el número es menor que 18")
+else:
+     print("El número es mayor o igual que 18")
